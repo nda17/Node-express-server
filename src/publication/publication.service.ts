b039bb03082bb1d1f3f4a19690753f064d69f6b8
unknown <khv1702@gmail.com>
2024-08-23 18:02:25 +0300

    Added service

---
+++ src/publication/publication.service.ts
@@ -0,0 +1,8 @@
+import { IPublication } from './publication.types'
+
+export class PublicationService {
+	createPublication({ publication }: IPublication) {
+		//Saving a publication to a database
+		return publication
+	}
+}
