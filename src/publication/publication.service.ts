544785ccae591f9a6a150f06f8732f86e1d4253a
unknown <khv1702@gmail.com>
2024-08-23 18:55:42 +0300

    Added prisma client

--- src/publication/publication.service.ts
+++ src/publication/publication.service.ts
@@ -1,7 +1,9 @@
 import { IPublication } from './publication.types'
 
+const prisma = new PrismaClient()
+
 export class PublicationService {
-	createPublication({ publication }: IPublication) {
+	createPublication({ publication }: IPublication): Publication {
 		//Saving a publication to a database
 		return publication
 	}
