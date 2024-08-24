7cda1b19be9ffc86209ead4b41a73c74e625f2a5
nfdmitry <khv1702@gmail.com>
2024-08-24 19:03:21 +0300

    Refactor

--- src/publication/publication.dto.ts
+++ src/publication/publication.dto.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod'
 
-export const createPublicationDto = z.object({
+export const publicationDto = z.object({
 	title: z.string().min(1, 'Title is required!').max(14),
 	description: z.string().min(5, 'Description is required!').max(30)
 })
