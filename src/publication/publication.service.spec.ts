7f83383374fc791271d137c33dfc97566cfc36ba
nfdmitry <khv1702@gmail.com>
2024-08-24 15:02:45 +0300

    Added entity service test

---
+++ src/publication/publication.service.spec.ts
@@ -0,0 +1,14 @@
+import { PublicationService } from './publication.service'
+
+describe('PublicationService', () => {
+	const publicationService = new PublicationService()
+
+	it('should create a publication', async () => {
+		const testPublication = await publicationService.createPublication({
+			title: 'Test title',
+			description: 'Test string'
+		})
+		expect(testPublication).toHaveProperty('id')
+		expect(testPublication.title).toEqual('Test title')
+	})
+})
