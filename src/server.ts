07386b0435fcc010a22f9d7d4df0d5e7d8616505
nfdmitry <khv1702@gmail.com>
2024-08-24 04:21:34 +0300

    Added main input file

---
+++ src/server.ts
@@ -0,0 +1,38 @@
+import { publicationRouter } from '@/publication/publication.controller'
+import logger from '@/utils/log'
+import { PrismaClient } from '@prisma/client'
+import 'colors'
+import dotenv from 'dotenv'
+import express from 'express'
+
+dotenv.config()
+const app = express()
+const prisma = new PrismaClient()
+
+const main = async () => {
+	app.use(logger)
+	app.use(express.json())
+	app.use('/api', publicationRouter)
+	app.all('*', (request, response) => {
+		response.status(404).json({ message: 'Not found' })
+	})
+
+	const PORT = process.env.PORT || 5000
+
+	app.listen(PORT, () =>
+		console.log(
+			`### Server running in ${process.env.NODE_ENV} mode at http://localhost:${PORT} ###`
+				.bgRed
+		)
+	)
+}
+
+main()
+	.then(async () => {
+		await prisma.$connect()
+	})
+	.catch(async (e) => {
+		console.error(e)
+		await prisma.$disconnect()
+		process.exit(1)
+	})
