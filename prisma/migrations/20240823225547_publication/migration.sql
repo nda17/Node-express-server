391409e9c29452111c7681378e6a7b7a1fba1fb3
nfdmitry <khv1702@gmail.com>
2024-08-24 04:16:26 +0300

    Added migration history files to the database.

---
+++ prisma/migrations/20240823225547_publication/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Publication" (
+    "id" SERIAL NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+
+    CONSTRAINT "Publication_pkey" PRIMARY KEY ("id")
+);
