15ae32f11816fc2eb74ea5b3575f30138177fac4
nfdmitry <khv1702@gmail.com>
2024-08-24 16:24:16 +0300

    Added get all items request

--- src/publication/publication.service.ts
+++ src/publication/publication.service.ts
@@ -4,9 +4,18 @@ import { PrismaClient, Publication } from '@prisma/client'
 export class PublicationService {
 	private prisma = new PrismaClient()
 
+	async getAll(): Promise<IPublication[]> {
+		return this.prisma.publication.findMany()
+	}
+
 	createPublication(publication: IPublication): Promise<Publication> {
-		return this.prisma.publication.create({
-			data: publication
-		})
+		try {
+			return this.prisma.publication.create({
+				data: publication
+			})
+		} catch (error) {
+			console.error(error)
+			throw new Error('Error while creating publication')
+		}
 	}
 }
